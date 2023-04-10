e5c736fa6bc3dca04170660f1672b4a44c4a12b2
heahminan <zerocustom_84@yahoo.com.sg>
2023-04-10 15:04:18 +0800

    base copy

---
+++ 3.1 Functions/script.js
@@ -0,0 +1,7 @@
+var main = function (input) {
+  var pi = 3.14;
+  var radius = 4;
+  var area = pi * radius * radius;
+  var myOutputValue = "Hello " + input + ", you look great today!";
+  return myOutputValue;
+};
