f6ad7e6e09be9929f9779b3782d745644f9c76f0
heahminan <zerocustom_84@yahoo.com.sg>
2023-04-10 17:06:18 +0800

    refactor juice wedding

--- 3.1 Functions/Juice Wedding/script.js
+++ 3.1 Functions/Juice Wedding/script.js
@@ -1,7 +1,6 @@
-var main = function (input) {
-  let numberOfGuests = input;
-  let numberOfOrangesNeeded = input * 4;
-  let litresOfOrangeJuice = input * 90 * 0.001;
+var main = function (numberOfGuests) {
+  let numberOfOrangesNeeded = numberOfGuests * 4;
+  let litresOfOrangeJuice = numberOfGuests * 90 * 0.001;
   let myOutputValue = `How many oranges are needed? Ans: ${numberOfOrangesNeeded} oranges How many litres of orange juice would I get? Ans : ${litresOfOrangeJuice} litres`;
   return myOutputValue;
 };
