9ca9115a2ae274d7d75ee7cb24f76887f070fa96
heahminan <zerocustom_84@yahoo.com.sg>
2023-04-10 15:22:48 +0800

    completed juice wedding

--- 3.1 Functions/script.js
+++ 3.1 Functions/script.js
@@ -1,7 +1,7 @@
 var main = function (input) {
-  var pi = 3.14;
-  var radius = 4;
-  var area = pi * radius * radius;
-  var myOutputValue = "Hello " + input + ", you look great today!";
+  let numberOfGuests = input;
+  let numberOfOrangesNeeded = input * 4;
+  let litresOfOrangeJuice = input * 90 * 0.001;
+  let myOutputValue = `How many oranges are needed? Ans: ${numberOfOrangesNeeded} oranges How many litres of orange juice would I get? Ans : ${litresOfOrangeJuice} litres`;
   return myOutputValue;
 };
